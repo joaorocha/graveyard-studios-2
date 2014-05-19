7b65ef98c24061c2aaa2340d00f5f956aa85463c
rickinbsb <rickinbsb@gmail.com>
2014-05-18 22:13:11 -0300

    Alteções no mail

--- main/mail.php
+++ main/mail.php
@@ -1,12 +1,16 @@
 <?php
+include('Mail.php');
+$mail = Mail::factory("mail");
+
 $name = $_POST['name'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 $graveyadmail = "contato@graveyard-studios.com";
 
-
 $message = wordwrap($message, 70, "\r\n");
-mail($graveyadmail, '[Formulario site] '.$subject, $message);
+
+$headers = array("From"=>$graveyadmail, "Subject"=>'[Formulario site] '.$subject);
+$mail->send($graveyadmail, $headers, $message);
 
 header( "Location: index.php" );
 ?>
