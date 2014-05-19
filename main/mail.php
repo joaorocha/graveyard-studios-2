ecce6890ece29b0063b13f04fa590f762bcd6bd1
rickinbsb <rickinbsb@gmail.com>
2014-05-18 22:31:48 -0300

    fix variable name

--- main/mail.php
+++ main/mail.php
@@ -3,14 +3,14 @@ include('Mail.php');
 $mail = Mail::factory("mail");
 
 $name = $_POST['name'];
-$mail = $_POST['mail'];
+$email = $_POST['mail'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 $graveyadmail = "contato@graveyard-studios.com";
 
 $message = wordwrap($message, 70, "\r\n");
 
-$headers = array("From"=>$mail, "Subject"=>'[Formulario site] '.$subject);
+$headers = array("From"=>$email, "Subject"=>'[Formulario site] '.$subject);
 $mail->send($graveyadmail, $headers, $message);
 
 header( "Location: index.php" );
